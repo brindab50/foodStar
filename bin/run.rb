da1d0fe085f907bd170dc62b9a3ec53803888ae8
Brinda Behal <brindaxo5@gmail.com>
2021-12-26 22:23:31 -0500

    add scraper

---
+++ bin/run.rb
@@ -0,0 +1,6 @@
+require_relative '../config/environment'
+
+
+cli_instance = CLI.new
+cli_instance.welcome
+
---
+++ config/environment.rb
@@ -0,0 +1,8 @@
+
+   
+require 'bundler'
+Bundler.require # use bundler to requiew everything in the gemfile
+
+ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: 'db/development.db')
+ActiveRecord::Base.logger = nil
+require_all 'app'
